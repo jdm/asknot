28c7f5ff9624892512dfb6c65fc66ee9a7974134
Josh Matthews <josh@joshmatthews.net>
2012-06-25 15:54:28 -0400

    Randomize group display.

--- foopy.js
+++ foopy.js
@@ -1,10 +1,11 @@
 var groupNode;
 var choiceIndex = [];
+var choices = [];
 var stack = [];
 
 function updateCurrentChoice() {
   var content = $('#content')[0];
-  content.innerHTML = $('.choices li', groupNode)[choiceIndex[choiceIndex.length - 1]].innerHTML;
+  content.innerHTML = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]].innerHTML;
   $('#next')[0].firstChild.textContent = chooseNegativeResponse();
 }
 
@@ -22,18 +23,30 @@ function chooseNegativeResponse() {
 }
 
 function switchGroup(group) {
+  groupNode = document.getElementById(group);
   if (!stack.length || stack[stack.length - 1] != group) {
     stack.push(group);
     choiceIndex.push(0);
+
+    //+ Jonas Raoni Soares Silva
+    //@ http://jsfromhell.com/array/shuffle [rev. #1]
+    function shuffle(v){
+      for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+      return v;
+    };
+
+    var c = [];
+    for (var i = 0; i < $('.choices li', groupNode).length; i++)
+      c.push(i);
+    choices.push(shuffle(c));
   }
   $('#back')[0].style.display = group == 'proglang' ? 'none' : 'block';
-  groupNode = document.getElementById(group);
   $('#question-display')[0].innerHTML = $('.question', groupNode)[0].outerHTML;
   updateCurrentChoice();
 }
 
 function investigate() {
-  var choice = $('.choices li', groupNode)[choiceIndex[choiceIndex.length - 1]];
+  var choice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
   if (choice.hasAttribute('next-group')) {
     switchGroup(choice.getAttribute('next-group'));
   } else {
@@ -44,6 +57,7 @@ function investigate() {
 function takeBack() {
   stack.splice(stack.length - 1, 1);
   choiceIndex.splice(choiceIndex.length - 1, 1);
+  choices.splice(choices.length - 1, 1);
   switchGroup(stack[stack.length - 1]);
 }
 
