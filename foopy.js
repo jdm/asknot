44ad9f71e19e6a9bab5576e4a0ce949974456b2b
Calvin Freitas <calvin.freitas@gmail.com>
2013-04-07 00:21:02 -0700

    retab JS

--- foopy.js
+++ foopy.js
@@ -1,131 +1,131 @@
 (function($) {
-var groupNode;
-var choiceIndex = [];
-var choices = [];
-var stack = [];
+    var groupNode;
+    var choiceIndex = [];
+    var choices = [];
+    var stack = [];
 
-function chooseNegativeResponse() {
-    var responses = $('.negative');
-    return responses[Math.floor(Math.random() * responses.length)];
-}
-
-function updateNegativeResponse() {
-    var negative = chooseNegativeResponse();
-    $($('.negative.visible')[0]).removeClass('visible');
-    $(negative).addClass('visible');  
-}
-
-function updateCurrentChoice(lastIndex) {
-    var lastChoice = $('.choices li', groupNode)[choices[choices.length - 1][lastIndex]];
-    var choice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
-    updateNegativeResponse();
-    lastChoice.style.display = 'none';
-    choice.style.display = 'inline';
-    $('#ok')[0].firstChild.href = choice.hasAttribute('next-group') ?
-    '' : choice.getAttribute('target');
-}
-
-function nextChoice() {
-    var lastIndex = choiceIndex[choiceIndex.length - 1];
-    choiceIndex[choiceIndex.length - 1]++;
-    if (choiceIndex[choiceIndex.length - 1] === $('.choices li', groupNode).length) {
-        choiceIndex[choiceIndex.length - 1] = 0;
+    function chooseNegativeResponse() {
+        var responses = $('.negative');
+        return responses[Math.floor(Math.random() * responses.length)];
     }
-    updateCurrentChoice(lastIndex);
-}
 
-function switchGroup(group) {
-    groupNode = document.getElementById(group);
-    if (!stack.length || stack[stack.length - 1] !== group) {
-        stack.push(group);
-        choiceIndex.push(0);
-
-        //+ Jonas Raoni Soares Silva
-        //@ http://jsfromhell.com/array/shuffle [rev. #1]
-        function shuffle(v) {
-            for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
-            return v;
-        };
-
-        var c = [];
-        for (var i = 0; i < $('.choices li', groupNode).length; i++)
-            c.push(i);
-        choices.push(shuffle(c));
+    function updateNegativeResponse() {
+        var negative = chooseNegativeResponse();
+        $($('.negative.visible')[0]).removeClass('visible');
+        $(negative).addClass('visible');  
     }
-    $('#back')[0].style.display = group == 'proglang' ? 'none' : 'block';
-    $('.question', groupNode)[0].style.display = 'block';
-    updateCurrentChoice(choiceIndex[choiceIndex.length - 1]);
-}
 
-function cleanUpCurrent() {
-    if (!groupNode) {
-        return;
+    function updateCurrentChoice(lastIndex) {
+        var lastChoice = $('.choices li', groupNode)[choices[choices.length - 1][lastIndex]];
+        var choice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
+        updateNegativeResponse();
+        lastChoice.style.display = 'none';
+        choice.style.display = 'inline';
+        $('#ok')[0].firstChild.href = choice.hasAttribute('next-group') ?
+            '' : choice.getAttribute('target');
     }
-    $('.question', groupNode)[0].style.display = 'none';
-    var lastChoice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
-    lastChoice.style.display = 'none';
-}
 
-function investigate(ev) {
-    ev.preventDefault();
-    var choice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
-    if (choice.hasAttribute('next-group')) {
-        cleanUpCurrent();
-        switchGroup(choice.getAttribute('next-group'));
-    } else {
-        window.open(choice.getAttribute('target'));
+    function nextChoice() {
+        var lastIndex = choiceIndex[choiceIndex.length - 1];
+        choiceIndex[choiceIndex.length - 1]++;
+        if (choiceIndex[choiceIndex.length - 1] === $('.choices li', groupNode).length) {
+            choiceIndex[choiceIndex.length - 1] = 0;
+        }
+        updateCurrentChoice(lastIndex);
     }
-}
 
-function takeBack() {
-    cleanUpCurrent();
-    stack.splice(stack.length - 1, 1);
-    choiceIndex.splice(choiceIndex.length - 1, 1);
-    choices.splice(choices.length - 1, 1);
-    switchGroup(stack[stack.length - 1]);
-}
+    function switchGroup(group) {
+        groupNode = document.getElementById(group);
+        if (!stack.length || stack[stack.length - 1] !== group) {
+            stack.push(group);
+            choiceIndex.push(0);
 
-function langChange() {
-    document.webL10n.setLanguage(this.value);
-}
+            //+ Jonas Raoni Soares Silva
+            //@ http://jsfromhell.com/array/shuffle [rev. #1]
+            function shuffle(v) {
+                for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+                return v;
+            };
 
-$(window).load(function() {
-    $('#ok a:first').on('click', investigate);
-    $('#next a:first').on('click', nextChoice);
-    $('#back a:first').on('click', takeBack);
-    $('#lang select').on('change', langChange);
+            var c = [];
+            for (var i = 0; i < $('.choices li', groupNode).length; i++)
+                c.push(i);
+            choices.push(shuffle(c));
+        }
+        $('#back')[0].style.display = group == 'proglang' ? 'none' : 'block';
+        $('.question', groupNode)[0].style.display = 'block';
+        updateCurrentChoice(choiceIndex[choiceIndex.length - 1]);
+    }
 
-    // Detected browser language
-    var browserLang = document.webL10n.getLanguage();
-    // Default language (value of the selected <option> element)
-    var defaultLang = $("#lang option:selected").val();
+    function cleanUpCurrent() {
+        if (!groupNode) {
+            return;
+        }
+        $('.question', groupNode)[0].style.display = 'none';
+        var lastChoice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
+        lastChoice.style.display = 'none';
+    }
 
-    if (defaultLang !== browserLang) {
-        var option = $('#lang option[value=' + browserLang + ']');
-        if (option.length) {
-            // If the browser language is supported, select the good option
-            option.attr('selected', 'selected');
+    function investigate(ev) {
+        ev.preventDefault();
+        var choice = $('.choices li', groupNode)[choices[choices.length - 1][choiceIndex[choiceIndex.length - 1]]];
+        if (choice.hasAttribute('next-group')) {
+            cleanUpCurrent();
+            switchGroup(choice.getAttribute('next-group'));
         } else {
-            // Else set the default language
-            document.webL10n.setLanguage(defaultLang);
+            window.open(choice.getAttribute('target'));
         }
     }
 
-    var query = window.location.hash.substring(1);
-    switchGroup('proglang');
-    if (query.length == 0) {
-      return;
+    function takeBack() {
+        cleanUpCurrent();
+        stack.splice(stack.length - 1, 1);
+        choiceIndex.splice(choiceIndex.length - 1, 1);
+        choices.splice(choices.length - 1, 1);
+        switchGroup(stack[stack.length - 1]);
     }
 
-    var node = document.querySelector('li[next-group="' + query + '"]');
-    if (!node)
-      return;
-    node = node.parentNode.parentNode;
-    while (node.has) {
-      
+    function langChange() {
+        document.webL10n.setLanguage(this.value);
     }
 
-    cleanUpCurrent();
-    switchGroup(query.substring(1));
-});
+    $(window).load(function() {
+        $('#ok a:first').on('click', investigate);
+        $('#next a:first').on('click', nextChoice);
+        $('#back a:first').on('click', takeBack);
+        $('#lang select').on('change', langChange);
+
+        // Detected browser language
+        var browserLang = document.webL10n.getLanguage();
+        // Default language (value of the selected <option> element)
+        var defaultLang = $("#lang option:selected").val();
+
+        if (defaultLang !== browserLang) {
+            var option = $('#lang option[value=' + browserLang + ']');
+            if (option.length) {
+                // If the browser language is supported, select the good option
+                option.attr('selected', 'selected');
+            } else {
+                // Else set the default language
+                document.webL10n.setLanguage(defaultLang);
+            }
+        }
+
+        var query = window.location.hash.substring(1);
+        switchGroup('proglang');
+        if (query.length == 0) {
+            return;
+        }
+
+        var node = document.querySelector('li[next-group="' + query + '"]');
+        if (!node)
+            return;
+        node = node.parentNode.parentNode;
+        while (node.has) {
+
+        }
+
+        cleanUpCurrent();
+        switchGroup(query.substring(1));
+    });
 })(window.jQuery);
